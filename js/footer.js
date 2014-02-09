d54206f748280535f148bc51555a2fcf81a94988
Fredrik Vraalsen <fredrik@vraalsen.no>
2014-02-09 21:07:11 +0100

    Flyttet header og footer til script

---
+++ js/footer.js
@@ -0,0 +1,17 @@
+document.write(
+  "    <footer>\n" +
+  "      <div class='container'>\n" +
+  "        <hr>\n" +
+  "        <div class='row'>\n" +
+  "          <div class='col-sm-3 text-center'><strong>Trening - lek - trivsel</strong></div>\n" +
+  "          <div class='col-sm-6 text-center'>\n" +
+  "            <address>\n" +
+  "              <strong>Manglerud Gymnastikkforening</strong><br>\n" +
+  "              Postboks 67 - 0612 Oslo<br>\n" +
+  "              <a href='mailto:post@manglerudgymnastikkforening.no'>post@manglerudgymnastikkforening.no</a>\n" +
+  "            </address>\n" +
+  "          </div>\n" +
+  "          <div class='col-sm-3 text-right'>Sist oppdatert 09.02.2014</div>\n" +
+  "        </div>\n" +
+  "      </div>\n" +
+  "    </footer>");
---
+++ js/header.js
@@ -0,0 +1,42 @@
+document.write(
+"    <nav class='navbar-wrapper navbar-default navbar-fixed-top' role='navigation'>\n" +
+"      <div class='container'>\n" +
+"        <div class='navbar-header'>\n" +
+"          <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#mgf-navbar-content'>\n" +
+"            <span class='sr-only'>Toggle navigation</span>\n" +
+"            <span class='icon-bar'></span>\n" +
+"            <span class='icon-bar'></span>\n" +
+"            <span class='icon-bar'></span>\n" +
+"          </button>\n" +
+"          <a class='navbar-brand visible-xs' href='#'><img src='images/MGF_navn.jpg' class='img-responsive' style='max-height: 25px'/></a>\n" +
+"        </div>\n" +
+"\n" +
+"        <div class='collapse navbar-collapse' id='mgf-navbar-content'>\n" +
+"          <ul class='nav navbar-nav'>\n" +
+"            <li class='active'><a href='index.html'><span class='glyphicon glyphicon-home'></span> Hjem</a></li>\n" +
+"            <li><a href='innmelding.html'><span class='glyphicon glyphicon-list-alt'></span> Innmelding og betaling</a></li>\n" +
+"            <li><a href='semesterplan.html'><span class='glyphicon glyphicon-calendar'></span> Semesterplan</a></li>\n" +
+"            <li class='dropdown'>\n" +
+"              <a href='#' class='dropdown-toggle' data-toggle='dropdown'><span class='glyphicon glyphicon-info-sign'></span> Informasjon</a>\n" +
+"              <ul class='dropdown-menu'>\n" +
+"                <li><a href='nyheter.html'>Nyheter</a></li>\n" +
+"                <li><a href='grasrotandelen.html'>Grasrotandelen</a></li>\n" +
+"              </ul>\n" +
+"            </li>\n" +
+"          </ul>\n" +
+"          <ul class='nav navbar-nav navbar-right'>\n" +
+"            <li><a href='mailto:post@manglerudgymnastikkforening.no'><span class='glyphicon glyphicon-envelope'> Kontakt oss</span></a></li>\n" +
+"          </ul>\n" +
+"        </div>\n" +
+"      </div>\n" +
+"    </nav>\n" +
+"\n" +
+"    <div class='container hidden-xs'>\n" +
+"      <div class='jumbotron'>\n" +
+"      </div>\n" +
+"    </div>\n" +
+"\n" +
+"    <div class='container visible-xs'>\n" +
+"      <div style='margin-top: 60px'>\n" +
+"      </div>\n" +
+"    </div>");
